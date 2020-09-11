c8e7a022748839b523a5ffa0a20d0353460d79bc
Dinesh Kumar <dotnext24@gmail.com>
2020-09-11 15:12:40 +0530

    back button and refresh

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Link,Switch,Route } from 'react-router-dom';
+import { BrowserRouter, Link,Switch,Route, withRouter, Prompt } from 'react-router-dom';
 
 import Home from './components/Home';
 import ContactDetail from './components/ContactDetail';
 import ContactList from './components/ContactList';
 
 
-function App() {
+class App extends React.Component {
+
+  componentDidMount(){
+    window.onbeforeunload = function() {
+     
+      return "";
+  }.bind(this);
+
+  }
+ 
+
+  //02
+  componentDidUpdate(){
+    const { history } = this.props;
+    window.onpopstate  = (e) => {
+      e.preventDefault();
+      if (window.confirm("Do you want to save your changes")) {
+        history.go(1);
+        // your custom logic to page transition,like react-router-dom history.push()
+         } else {
+          window.history.pushState(null, null, window.location.pathname);
+          
+     }
+   }
+  
+      }
+  render(){
   return (
     <React.Fragment>
+      
+
       <BrowserRouter>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <a className="navbar-brand" href="#">Logo</a>
@@ -46,6 +74,7 @@ function App() {
       </BrowserRouter>
     </React.Fragment>
   )
+  }
 }
 
-export default App;
+export default  withRouter(App);
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-
+import todos from './a.json'
 export default class Home extends Component {
+    
+    
     render() {
         return (
             <div>
-                <h1>Home</h1>
+                <h1>Home {todos.length}</h1>
             </div>
         )
     }
